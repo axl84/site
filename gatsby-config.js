266374919aa30e6e7087f5cbc843b3c3431c5c27
axl84 <axl84@users.noreply.github.com>
2018-04-25 03:32:26 +0300

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,62 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: 'Axl84 PWA 🐎🚀',
+    shortName: 'Axl84',
+    description:
+      'Just a Kyiv ninja site.',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-graphcms',
+      options: {
+        endpoint: process.env.GATSBY_GRAPHQLCMS_ENDPOINT,
+        token: process.env.GATSBY_GRAPHQLCMS_TOKEN,
+        // Get all remote data
+        query: `{
+          allPosts {
+            id
+            slug
+            title
+            content
+            dateAndTime
+            coverImage {
+              handle
+            }
+          }
+          allAuthors {
+            id
+            name
+            bibliography
+            avatar {
+              handle
+            }
+          }
+        }`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        get name() {
+          return module.exports.siteMetadata.title;
+        },
+        get short_name() {
+          return module.exports.siteMetadata.shortName;
+        },
+        start_url: '/',
+        background_color: '#fafafa',
+        theme_color: '#212121',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-netlify',
+  ],
+};
---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Typography, Grid } from 'material-ui';
+import Section from '../components/Section';
+
+const NotFoundPage = () => (
+  <Section>
+    <Grid item xs={12} sm={8}>
+      <Typography variant="display1">ТУТ НИЧЕГО НЕТ 🤦‍♂️</Typography>
+      <Typography>
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </Typography>
+    </Grid>
+  </Section>
+);
+
+export default NotFoundPage;
